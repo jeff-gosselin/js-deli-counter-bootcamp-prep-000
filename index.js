81d42cb94c7262f1b58d9b1095e178e1c145f7f5
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-08-27 22:54:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function takeANumber(katzDeliLine, newPerson) {
   return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`;
 }
 
+
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
