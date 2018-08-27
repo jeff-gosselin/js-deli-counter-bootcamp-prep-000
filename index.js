d4022bab018e3944fe6e22b728053c9d6f6575a4
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-08-27 22:59:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,16 +15,15 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   var people = [];
+  
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   }
   
   for (let i = 0; i < katzDeliLine.length; i++) {
-    
    people.push(` ${i+1}. ${katzDeliLine[i]}`);
-    
-  
   }  
+  
   people.join(',');
   return `The line is currently:${people}`;
   
