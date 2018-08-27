e136560fef4d67ebd54e31e4c89e9fa40ae78c4d
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-08-27 22:55:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   }
-  
   var currentlyServing = katzDeliLine.shift();
   return `Currently serving ${currentlyServing}.`;
 }
